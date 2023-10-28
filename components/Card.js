4340b7c63341c28d3722ed1fffce3bcdc24f24af
jaypopat <contact.jaypopat@gmail.com>
2023-10-28 15:39:44 +0100

    added chnages to card ui

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import HoverVideoPlayer from 'react-hover-video-player';
 
-const Card = ({ video, price }) => (
+const Card = ({ image, video, price }) => (
     <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
-      <div className="md:flex">
+      <div className="md:flex-col">
         <div className="md:flex-shrink-0">
-          <video className="h-48 w-full object-cover md:w-48" src={video} controls />
+          <HoverVideoPlayer
+            videoSrc={video}
+            pausedOverlay={
+              <img
+                src={image}
+                alt=""
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+          />
         </div>
         <div className="p-8">
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Price</div>
@@ -12,5 +31,5 @@ const Card = ({ video, price }) => (
         </div>
       </div>
     </div>
-  );
+);
 export default Card;
