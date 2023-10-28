f5af6b4523bad49ac2c780f86f7f885c30c4be6f
Olubusayo Amowe <amoweolubusayo@gmail.com>
2023-10-28 17:32:35 +0100

    video updated

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -10,38 +10,37 @@ const data = [
     video:
       "https://github.com/amoweolubusayo/1UP/assets/20168921/c9347fbc-9c3b-4514-9f2f-651a3773bbde",
     price: "12 TXZ",
-    name: "Chelsea Mixed Reality Tshirt"
+    name: "Chelsea Mixed Reality Tshirt",
   },
   {
     video:
       "https://github.com/amoweolubusayo/1UP/assets/20168921/cc8570ba-3eec-4d3f-a552-1e8f37145cf5",
     price: "25 TXZ",
-    name : "Manchester City Mixed Reality Buses"
+    name: "Manchester City Mixed Reality Buses",
   },
   {
     video:
       "https://github.com/amoweolubusayo/1UP/assets/20168921/b5f6e8ab-e37a-4b09-8e56-cb62d2c443ee",
     price: "19 TXZ",
-    names: "Nike X RTFKT Mixed Reality AirJordan"
+    names: "Nike X RTFKT Mixed Reality AirJordan",
   },
   {
     video:
       "https://github.com/amoweolubusayo/1UP/assets/20168921/c7c8a90f-a993-4c0b-997c-17852d5bbf4e",
     price: " 14 TXZ",
-    name : "Jamie Oliver - Personal Nutrionist"
-
+    name: "Jamie Oliver - Personal Nutrionist",
   },
   {
     video:
       "https://github.com/amoweolubusayo/1UP/assets/20168921/f28c73e5-324d-4b0b-8fcc-c999c1438100",
     price: "14 TXZ",
-    name : "Serena Williams - Personal Tennis Coach"
+    name: "Serena Williams - Personal Tennis Coach",
   },
   {
     video:
-      "https://github.com/amoweolubusayo/1UP/assets/20168921/d6a019fd-168f-4ed9-90b4-a67d28dc735b",
+      "https://github.com/amoweolubusayo/1UP/assets/20168921/d7909669-027a-4d73-bde1-f0685249caa5",
     price: "14 TXZ",
-    name : "Barack Obama - Life Coach"
+    name: "Barack Obama - Life Coach",
   },
 ];
 export default function LandingPage() {
@@ -62,7 +61,12 @@ export default function LandingPage() {
         </div> */}
       <div className="grid grid-cols-3 gap-4 p-4">
         {data.map((item, index) => (
-          <Card key={index} video={item.video} price={item.price} name = {item.name} />
+          <Card
+            key={index}
+            video={item.video}
+            price={item.price}
+            name={item.name}
+          />
         ))}
       </div>
       <Features />
