7f812cdec730ea4a6ab81dce7294d2b01e0744a6
Olubusayo Amowe <amoweolubusayo@gmail.com>
2023-10-29 01:17:44 +0100

    buy function

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,43 @@
 import React from "react";
 import HoverVideoPlayer from "react-hover-video-player";
+import { TezosToolkit } from "@taquito/taquito";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import { NetworkType } from "@airgap/beacon-sdk";
+
+const rpcUrl = "https://ghostnet.ecadinfra.com";
+const Tezos = new TezosToolkit(rpcUrl);
+console.log("Tezos is", Tezos);
+const network = NetworkType.GHOSTNET;
+
+const buyNFT = async () => {
+  console.log("here")
+  try {
+    const contract = await Tezos.wallet.at(
+      "KT19sQSJsMzgsQCp4LCdd9C7T5wXse2A79Rn"
+    );
+
+    const newWallet = new BeaconWallet({
+      name: "Simple dApp tutorial",
+      preferredNetwork: network,
+    });
+    await newWallet.requestPermissions({
+      network: { type: network, rpcUrl },
+    });
+
+    address = await newWallet.getPKH();
+
+    const userBalance = await Tezos.tz.getBalance(address);
+    console.log("userbalance is", userBalance);
+    console.log("contract is", contract);
+    const operation = await contract.methods
+      .buy("0.1", address)
+      .send();
+    await operation.confirmation();
+    console.log(`NFT bought by ${address}`);
+  } catch (error) {
+    console.error("Error buying NFT:", error);
+  }
+};
 
 const Card = ({ image, name, video, price }) => (
   <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
@@ -30,8 +68,8 @@ const Card = ({ image, name, video, price }) => (
           {name}
         </div>
         <p className="mt-2 text-gray-500">{price}</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-          Buy
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={buyNFT}>
+          Buy 
         </button>
       </div>
     </div>
